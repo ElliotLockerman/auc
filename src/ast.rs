0cd5214e75c39a79fd3d384d2f8660e73c1ecc73
Elliot Lockerman <elliot@lockerman.info>
2018-05-13 15:40:58 -0400

    4 functions work

---
+++ src/ast.rs
@@ -0,0 +1,42 @@
+/*
+Modern Compilers in C p.45
+E = expression
+F = factor (things you multiply)
+T = term (things you add)
+
+E -> E + T
+E -> E - T
+E -> T
+
+T -> T * F
+T -> T / F
+T -> F
+
+F -> num
+F -> ( E )
+
+*/
+
+#[derive(Debug, Clone)]
+pub enum Expression {
+    Sum(Box<Expression>, Box<Term>),
+    Diff(Box<Expression>, Box<Term>),
+    Term(Box<Term>),
+}
+
+#[derive(Debug, Clone)]
+pub enum Term {
+    Prod(Box<Term>, Box<Factor>),
+    Quot(Box<Term>, Box<Factor>),
+    Factor(Box<Factor>),
+}
+
+#[derive(Debug, Clone)]
+pub enum Factor {
+    Num(f64),
+    Exp(Box<Expression>),
+}
+
+
+
+
---
+++ src/main.rs
@@ -0,0 +1,67 @@
+
+
+
+extern crate rustyline;
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+pub mod grammar;
+pub mod ast;
+
+use ast::*;
+
+
+fn main() {
+    let mut rl = Editor::<()>::new();
+
+    loop {
+        let readline = rl.readline("> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(&line);
+                if line.chars().into_iter().all(|ch| ch.is_whitespace()) {
+                    continue; 
+                }
+                let exp = match grammar::ExpParser::new().parse(&line) {
+                    Ok(p) => p,
+                    Err(e) => {
+                        eprintln!("{}", e);
+                        continue;
+                    },
+                };
+       
+                println!("{}", eval_exp(&exp));
+            },
+            Err(ReadlineError::Interrupted)| Err(ReadlineError::Eof) => break,
+            Err(err) => println!("Error: {:?}", err),
+        }
+    }
+}
+
+
+
+fn eval_exp(exp: &Expression) -> f64 {
+    match exp {
+        Expression::Sum(lhs, rhs) => eval_exp(lhs) + eval_term(rhs),
+        Expression::Diff(lhs, rhs) => eval_exp(lhs) - eval_term(rhs),
+        Expression::Term(term) => eval_term(term),
+    }
+}
+
+fn eval_term(term: &Term) -> f64 {
+    match term {
+        Term::Prod(lhs, rhs) => eval_term(lhs) * eval_fact(rhs),
+        Term::Quot(lhs, rhs) => eval_term(lhs) / eval_fact(rhs),
+        Term::Factor(fact) => eval_fact(fact),
+    }
+}
+
+fn eval_fact(fact: &Factor) -> f64 {
+    match fact {
+        Factor::Num(n) => *n,
+        Factor::Exp(e) => eval_exp(e),
+    }
+}
+
+
+
